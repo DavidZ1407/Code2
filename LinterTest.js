bd4761c1a60db46d2a23a3fe3249c224323e7d0b
DavidZ1407 <149605583+DavidZ1407@users.noreply.github.com>
2025-10-23 14:12:09 +0200

    delete test.ts

--- LinterTest.js
+++ LinterTest.js
@@ -2,19 +2,20 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var linterTest;
 (function (linterTest) {
-    let info = { text: "G`udetmvhsgBncd1 ", key: key.Pos };
-    console.log(DeCrypt(info.text, info.key));
-    function DeCrypt(text, _Key) {
-        const result = "";
-        for (let i = 0; i < text.length(); i + +)
-            ;
-        result += String.fromCharCode(text.charCodeAt(i) + _Key);
-        return result;
-    }
     let key;
     (function (key) {
         key[key["Pos"] = 1] = "Pos";
         key[key["neg"] = -1] = "neg";
     })(key || (key = {}));
+    let info = { text: "G`udetmvhsgBncd1 ", key: key.Pos };
+    console.log(DeCrypt(info.text, info.key));
+    function DeCrypt(text, _Key) {
+        let result = "";
+        for (let i = 0; i < text.length; Number())
+            i++;
+        result += String.fromCharCode;
+        (text.charCodeAt(text.length) + _Key);
+        return result;
+    }
 })(linterTest || (linterTest = {}));
 //# sourceMappingURL=LinterTest.js.map
