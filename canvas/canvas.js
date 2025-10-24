cfe9a6ec98f29f4a827e73bb7c5ac84746687d2e
DavidZ1407 <149605583+DavidZ1407@users.noreply.github.com>
2025-10-24 15:22:33 +0200

    canvas

---
+++ canvas/canvas.js
@@ -0,0 +1,64 @@
+"use strict";
+var Canvas;
+(function (Canvas) {
+    window.addEventListener("load", handleLoad);
+    let crc2;
+    let imgData;
+    function handleLoad(_event) {
+        let canvas = document.querySelector("canvas");
+        if (!canvas)
+            return;
+        crc2 = canvas.getContext("2d");
+        crc2.fillStyle = "#3b1616ff";
+        imgData = crc2.getImageData(0, 0, crc2.canvas.width, crc2.canvas.height);
+        for (let i = 0; i < 1; i++) {
+            const centerX = Math.random() * canvas.width;
+            const centerY = Math.random() * canvas.height;
+            const radius = Math.random() * Math.min(canvas.width, canvas.height) / 2;
+            drawCurvedLines(centerX, centerY, radius);
+            drawShapes(centerX, centerY, radius);
+            // window.setInterval()
+        }
+        function drawCurvedLines(centerX, centerY, radius) {
+            for (let i = 0; i < 360; i += 2) {
+                crc2.save();
+                crc2.translate(centerX, centerY);
+                crc2.rotate((i * Math.PI) / 180);
+                crc2.translate(-centerX, -centerY);
+                crc2.strokeStyle = "red";
+                crc2.lineWidth = Math.random() * 30;
+                crc2.beginPath();
+                crc2.moveTo(centerX, centerY);
+                crc2.bezierCurveTo(centerX + radius, centerY, centerX + radius, centerY + radius, centerX, centerY + radius);
+                crc2.stroke();
+                crc2.restore();
+            }
+        }
+        function drawShapes(centerX, centerY, radius) {
+            for (let i = 0; i < 360; i += 2) {
+                crc2.save();
+                crc2.translate(centerX, centerY);
+                crc2.rotate((i * Math.PI) / 180);
+                crc2.translate(-centerX, -centerY);
+                crc2.lineWidth = Math.random() * 15;
+                if (i % 2 === 0) {
+                    crc2.beginPath();
+                    crc2.arc(centerX, centerY, radius, 100, 0 * Math.PI);
+                    crc2.stroke();
+                }
+                else {
+                    crc2.beginPath();
+                    crc2.moveTo(centerX - radius, centerY);
+                    crc2.lineTo(centerX + radius, centerY);
+                    crc2.stroke();
+                }
+                crc2.restore();
+            }
+        }
+    }
+    function redraw() {
+        crc2.clearRect(0, 0, crc2.canvas.width, crc2.canvas.height);
+        crc2.putImageData(imgData, 0, 0);
+    }
+})(Canvas || (Canvas = {}));
+//# sourceMappingURL=canvas.js.map
