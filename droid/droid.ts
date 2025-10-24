794161fde907ab5c7174693f3f4f05ab0769d981
DavidZ1407 <149605583+DavidZ1407@users.noreply.github.com>
2025-10-24 11:34:43 +0200

    droid 1,2,3 task

--- droid/droid.ts
+++ droid/droid.ts
@@ -12,16 +12,55 @@ export namespace Droid {
     }
 
 
+    let random: number = 0
 
-    export function getCommand(): Command{
+
+
+    export function getCommand(): Command {
+
         const command: Command = {
             module: "Chassis",
             method: "move",
-            data: "left",
+            data: "stop",
         }
+        random = Math.floor(Math.random() * 5)
+
+        switch (random) {
+            case 0:
+                command.data = "forward"
+                break;
+
+            case 1:
+                command.data = "back"
+                break;
+
+            case 2:
+                command.data = "left"
+                break;
+
+            case 3:
+                command.data = "right"
+                break;
+
+            case 4:
+                command.data = "stop"
+                break;
+        }
+
+
+        // if (number % 2 === 0) {
+        //     command.data = "forward";
+        //     number++;
+        // } else {
+        //     command.data = "left";
+        //     number++;
+        // }
+
+
         console.log(command);
         return command;
-    } 
+
+    }
 
 
 
